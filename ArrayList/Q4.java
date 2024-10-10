965bf169b35e66164d2b67bf8f03bee50fadf52d
shubhanshu <shubhanshumohanty@gmail.com>
2024-10-10 07:38:30 +0530

    arrayList 4th question

---
+++ ArrayList/Q4.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Q4 {
+    /*
+     * ArrayList Traversal
+     */
+    public static void listTraversal(ArrayList<Integer> numbers) {
+        // code here
+        for (int n : numbers) {
+            System.out.print(n + " ");
+        }
+    }
+}
